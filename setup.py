5869b0338d616027c525d0c2fd8191e636b42687
Maris Erts <7067057+mariserts@users.noreply.github.com>
2022-03-31 11:52:18 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
 
     # Dependent packages (distributions)
     install_requires=[
-        "Django==4.0.1",
+        "django>=4.0.2",
         "djantic==0.4.1",
         "pydantic==1.9.0",
     ],
